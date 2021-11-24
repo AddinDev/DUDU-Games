56ccd5790cd48f3cb217b1f3d924980519604d5e
FathanAkram-app <fathan.a.dev@gmail.com>
2021-11-24 23:37:23 +0700

    little design and go to detail

--- games/js/game-list-data.js
+++ games/js/game-list-data.js
@@ -2,51 +2,13 @@ const games = [
     {
         title: "Plant Cells",
         desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=1"
+        image: "game-files/plant-cells/thumbnail.jpeg",
+        gamepage: "game-files/plant-cells/"
     },
     {
         title: "Math Maze",
         desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=11"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=3"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=4"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=5"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=6"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=7"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=8"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=9"
-    },
-    {
-        title: "Game 1",
-        desc: "Tentang",
-        image: "https://picsum.photos/1000/1000/?random=10"
-    },
+        image: "game-files/math-maze/thumbnail.jpeg",
+        gamepage: "game-files/math-maze/"
+    }
 ];
--- games/js/game-list-main.js
+++ games/js/game-list-main.js
@@ -6,14 +6,20 @@ const main = function () {
         const {
             title,
             desc,
-            image
+            image,
+            gamepage
         } = game;
         const gameElement = document.createElement("div");
-        gameElement.innerHTML = `<a href="detail/" style="text-decoration: none;"><div class="grid-item hoverFocus" style="background-image: url(${image})">
-              <h4>${title}</h4>
-              <p>${desc}</p>
-              </div>
-              </a>`;
+        gameElement.innerHTML = 
+        `
+        <a href="${gamepage}" style="text-decoration: none;">
+            <div class="grid-item hoverFocus" style="background-image: url(${image})">
+                <div class="textItems">
+                    <h4 class="textItems" style="background-color:white;">${title}</h4>
+                </div>
+                
+            </div>
+        </a>`;
         gameListElement.appendChild(gameElement);
     });
 };
