08781c8c4a6fa9cebf1f9633583de8ba53bf97e5
FathanAkram-app <fathan.a.dev@gmail.com>
2021-11-27 21:05:30 +0700

    list fixes

--- games/js/game-list-data.js
+++ games/js/game-list-data.js
@@ -10,5 +10,245 @@ const games = [
         desc: "Tentang",
         image: "game-files/math-maze/thumbnail.jpeg",
         gamepage: "game-files/math-maze/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Bubble Crush",
+        desc: "Tentang",
+        image: "https://cdn.education.com/files/static/game-images/number-pairs-bubble-buster-thumbnail.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Kids Science",
+        desc: "Tentang",
+        image: "https://image.winudf.com/v2/image1/Y29tLnByZXNjaG9vbGVkdWNhdGlvbmdhbWVzLmtpZHMuc2NpZW5jZWxlYXJuaW5nLmVkdWNhdGlvbi5saXZpbmdsYW5kbm9ubGl2aW5nX3NjcmVlbl82XzE1NjUyOTU2MTFfMDEx/screen-6.jpg?fakeurl=1&type=.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Dog Chef",
+        desc: "Tentang",
+        image: "https://tryengineering.org/wp-content/uploads/education.com-games.jpg",
+        gamepage: "game-files/notavailable/"
+    },
+    {
+        title: "Word Kids",
+        desc: "Tentang",
+        image: "https://cdn.cloudflare.steamstatic.com/steam/apps/1387560/capsule_616x353.jpg?t=1599725390",
+        gamepage: "game-files/notavailable/"
     }
 ];
